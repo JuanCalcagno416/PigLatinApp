714858d0ebd02bd509042aefeed18d8440fd8f0f
Juan Calcagno <negro32@gmail.coma>
2018-05-31 15:40:46 +0200

    Initial Commit

--- piglatinapp.api/controllers/auth_controller.js
+++ piglatinapp.api/controllers/auth_controller.js
@@ -8,7 +8,12 @@ module.exports = {
         User.findOne({$or:[{"email":req.body.email},{"username":req.body.username}]})
             .then(user => {
                 if(user != null){
-
+                    if(user.email == req.body.email || user.username == req.body.username) {
+                        console.log('logeando')
+                    } else {
+                        console.log("No es el correo o el username")
+                    }
+                    
                 } else {
                     return res.json({"Error":"InvalidCredentials"})
                 }

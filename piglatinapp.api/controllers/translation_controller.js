f02327ff015b43402a5a5c2c0bdb1cf6f539b5e7
Juan Calcagno <negro32@gmail.coma>
2018-06-01 17:20:31 +0200

    Translation is working with one word, 'QU', now working with sentences

--- piglatinapp.api/controllers/translation_controller.js
+++ piglatinapp.api/controllers/translation_controller.js
@@ -3,7 +3,7 @@ import Translation from '../models/translation_model';
 module.exports = {
 
     translate: (req,res) => {
-      Translation.translateWord (req.body.word,req.currentUser,(err,translation) => {
+      Translation.translateWord (req.body.text,req.currentUser,(err,translation) => {
             if(err) return res.json(err);
             if(translation) return res.json(translation);
       });
--- piglatinapp.api/models/translation_model.js
+++ piglatinapp.api/models/translation_model.js
@@ -1,8 +1,8 @@
 import mongoose     from 'mongoose'
 
 const TranslationSchema = new mongoose.Schema({
-    oldWord:    {type:String},
-    newWord:    {type:String},
+    oldText:    {type:String},
+    newText:    {type:String},
     userId:     {type:mongoose.Schema.ObjectId,ref:'User'},
 
     created_at: {
@@ -13,31 +13,103 @@ const TranslationSchema = new mongoose.Schema({
 
 TranslationSchema.statics = {
 
-    translateWord: (wordToTranslate,user,cb) =>{
-        let translation = new Translation({
-            oldWord:wordToTranslate.toLowerCase()
-        });
-        translation.userId = user._id
+    translateWord: (textToTranslate,user,cb) => {
 
-        let firstLetter = translation.oldWord.charAt(0);
+        if (/\s/.test(textToTranslate)) var splitString = textToTranslate.split(" ");
+        console.log(splitString)
 
+        if(splitString == undefined){
+            
+            let firstLetter = textToTranslate.charAt(0)
+
+            Translation.checkFirstLetter(firstLetter,textToTranslate.toLowerCase(),(completeWord) => {
+        
+                let translation = new Translation({
+                    oldText:textToTranslate.toLowerCase(),
+                    newText:completeWord
+                });
+                translation.userId = user._id
+
+                cb(null,translation)
+
+            });
+        }else {
+            
+        }
+
+    },
+
+
+    checkFirstLetter: (firstLetter,text,cb) => {
+        console.log("Checking first letter");
         switch (firstLetter) {
             case "a":
+            Translation.vocalMethod(text,cb)
             break;
             case "e":
+            Translation.vocalMethod(text,cb)
             break;
             case "i":
-            console.log('Vocal');
+            Translation.vocalMethod(text,cb)
                 break;
             case "o":
+            Translation.vocalMethod(text,cb)
                 break;
             case "u":
+            Translation.vocalMethod(text,cb)
                 break;
             default :
-            console.log('Consonant');
+            Translation.consonantMethod(text,cb)
             break;
         }
+    },
+
+    vocalMethod: (text,cb) => {
+        console.log("Vocal method, letter ->   " + text)
+        return cb(text+"way")
+    },
+
+    consonantMethod: (completeWord,cb) => {
+        let savedConsonantLetters = [];
+        let i = 0;
+
+        console.log("Consonant method, letter ->   " + completeWord)
+
+        // Check word until it for letters 
+
+        while (i<completeWord.length){
+            let l = completeWord.charAt(i);
+
+            // Check for vocal  
+            if(l == "a" || l == "e" || l == "i" || l == "o" || l == 'u') {
+                console.log('Loop - ' +i+' se topo con una vocal');
+                if(savedConsonantLetters[0] == 'q' && l == 'u'){
+                    savedConsonantLetters.push(l);
+                    completeWord = completeWord.replace(l,'');
+                }else {
+                    break
+                }
+            }else {
+                // Check for consonants, delete letters in word and add them to an array                
+                savedConsonantLetters.push(l);
+                completeWord = completeWord.replace(l,'');
+            }
+        }
+    
+
+        // Append consonant letters that it found before first vocal
+        for(var j = 0;j < savedConsonantLetters.length; j++) {
+            console.log(savedConsonantLetters[j])
+            completeWord = completeWord + savedConsonantLetters[j];
+        }
+        
+        // Adding 'ay'
+            completeWord = completeWord + 'ay';
+
+        console.log(completeWord)
+        return cb(completeWord)
     }
+
 
 }
 
